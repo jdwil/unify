c1c37fdb15b319954d21323a2b09d6df62f7014c
JD Williams <jd@easygm.com>
2017-06-23 17:54:08 -0500

    Added License info.

--- src/Exception/XdebugException.php
+++ src/Exception/XdebugException.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Copyright (c) 2017 - 2017 JD Williams
+ *
+ * This file is part of Unify, a PHP testing framework built by JD Williams. Unify is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+ *
+ * Unify is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU Lesser General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * You should have received a copy of the GNU General Public License along with Unify. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace JDWil\Unify\Exception;
 
